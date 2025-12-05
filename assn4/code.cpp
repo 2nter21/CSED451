b2118b6dd07a34bd7524f3fcf0b5f9ea6d416bb9
2nter21 <155376343+2nter21@users.noreply.github.com>
2025-12-06 02:11:23 +0900

    [Feat] Texture file loading
    - 파일 경로 수정
    - 텍스쳐 파일 로딩 기능 구현

--- assn4/code.cpp
+++ assn4/code.cpp
@@ -13,6 +13,39 @@
 #include <iostream>
 #define TINYOBJLOADER_IMPLEMENTATION
 #include "tiny_obj_loader.h"
+#define STB_IMAGE_IMPLEMENTATION
+#include "std_image.h"
+
+// Texture loader. returns LG texture id
+unsigned int loadTexture(const char* path) {
+    int width, height, nrChannels;
+    stbi_set_flip_vertically_on_load(true);
+    unsigned char *data = stbi_load(path, &width, &height, &nrChannels, 0);
+    if (!data) {
+        std::cerr << "Failed to load texture: " << path << std::endl;
+        return 0;
+    }
+
+    GLenum format = GL_RGB;
+    if (nrChannels == 1) format = GL_RED;
+    else if (nrChannels == 3) format = GL_RGB;
+    else if (nrChannels == 4) format = GL_RGBA;
+
+    unsigned int tex;
+    glGenTextures(1, &tex);
+    glBindTexture(GL_TEXTURE_2D, tex);
+
+    glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
+    glGenerateMipmap(GL_TEXTURE_2D);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    stbi_image_free(data);
+    return tex;
+}
 
 class Shader {
 public:
@@ -1035,6 +1068,13 @@ int main(int argc, char** argv) {
     squareModel.load("assets/square.obj");
     starModel.load("assets/star.obj");
     triangleModel.load("assets/triangle.obj");
+
+    // load texture files
+    unsigned int diffuseSonic1 = loadTexture("new_assets/diffuse_sonic_1.png");
+    unsigned int diffuseSonic2 = loadTexture("new_assets/diffuse_sonic_2.png");
+    unsigned int diffuseStarship = loadTexture("new_assets/diffuse_starship.png");
+    unsigned int normalCobble = loadTexture("new_assets/normal_cobble.png");
+
 
     // intialize node pool
     orbitEntityNodePool.resize(MAX_ORBIENTITIES);
